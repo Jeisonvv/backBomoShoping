f21aee9ca247cdb464d05c5b1b89181f63a42a27
jeisonvv <jeisonhvargaav@gmail.com>
2025-01-28 23:05:07 -0500

    colocando el comando start

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const { connectDB } = require('./src/config/condb');
+
 const appMogoShoping = require('./src/server');
-const {initializeReactionListener, } = require('./src/services/whatsappService');
 
 const PORT = process.env.PORT || 3000;
 
 const HOST = process.env.HOST 
 
 connectDB().then(() => {
-    appMogoShoping.listen(PORT, HOST, () => {
+    appMogoShoping.listen(PORT, () => {
         console.log(`Server running on http://${HOST}:${PORT}`);
        
     });
